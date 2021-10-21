ca0fe363fe96b33acf60384d6fa6a228b2731d93
norimicia <mitu1023ki0830@gmail.com>
2021-10-21 17:37:41 +0900

    手順4オブジェクト同期まで完了

--- Assets/Scripts/AvataController.cs
+++ Assets/Scripts/AvataController.cs
@@ -1,8 +1,16 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AvataController : MonoBehaviourPunCallbacks        // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
+public class AvataController : MonoBehaviourPunCallbacks ,IPunObservable      // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
 {
+    private const float MaxStamina = 6f;       //constは、その値を固定するという意味(変数)
+
+    [SerializeField]
+    private Image staminaBar = default;
+
+    private float currentStamina = MaxStamina;
+
     void Update()
     {
         // 自身が生成したオブジェクトだけに移動処理を行う
@@ -10,7 +18,31 @@ public class AvataController : MonoBehaviourPunCallbacks        // MonoBehaviour
         {
             Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
 
-            transform.Translate(6 * Time.deltaTime* input.normalized);
+            if(input.sqrMagnitude > 0)      //そもそもMagnitudeはVector3の各値を2乗して足してルートする。sqrをつけたほうが早い
+            {
+                currentStamina = Mathf.Max(0f, currentStamina - Time.deltaTime);        //大きいほうの値を返す。currentStaminaが0以下になるときは0のほうを返す
+
+                transform.Translate(6 * Time.deltaTime * input.normalized);
+            }
+            else
+            {
+                currentStamina = Mathf.Min(currentStamina + Time.deltaTime*2,MaxStamina);       //大きいほうの値を返す。currentStaminaがMaxStamina以下になるときはMaxStaminaのほうを返す
+            }
+        }
+
+        //スタミナをゲージに反映させる
+        staminaBar.fillAmount = currentStamina / MaxStamina;
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)      //第一引数が動きがあったゲームオブジェクト、第二引数はわからない
+    {
+        if (stream.IsWriting)       //自分（動く方）が情報を送る側の時
+        {
+            stream.SendNext(currentStamina);
+        }
+        else
+        {
+            currentStamina = (float)stream.ReceiveNext();
         }
     }
 }
